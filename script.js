efde76dae7ab93d5db0f40c33b340b80dfc3b403
Josue-Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-03-09 11:38:08 -0800

    First Commit (js, css, html files)

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(('.skills'), {
+    strings : ['Software Engineer','Developer','Options Trader'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
